11e687354ef45af82b56bb40553af4842cf88dc1
SeuongwooKim <suong22@naver.com>
2025-07-10 15:07:36 +0900

    first commit

---
+++ src/main/java/com/example/tossback/TossBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tossback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tossback/TossBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tossback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
