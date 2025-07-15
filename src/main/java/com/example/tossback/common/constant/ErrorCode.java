f3075738f505a08b4ff1e7869058194f8c510022
ingoo <ryan0524@naver.com>
2025-07-15 09:26:24 +0900

    초기 공통설정 생각나는대로 ..일단푸쉬..

---
+++ src/main/java/com/example/tossback/common/constant/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.example.tossback.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorCode {
+    public static final String NO_ERROR = "0000";
+}
---
+++ src/main/java/com/example/tossback/common/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.example.tossback.common.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private LocalDateTime deletedAt;
+}
