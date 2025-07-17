5fa9788dd4a09a85c4e4108541510fbd65fa6d89
ingoo <ryan0524@naver.com>
2025-07-17 10:55:58 +0900

    시큐리티 + JWT 공통적인 설정 완료

--- src/main/java/com/example/tossback/member/controller/MemberController.java
+++ src/main/java/com/example/tossback/member/controller/MemberController.java
@@ -1,4 +1,32 @@
 package com.example.tossback.member.controller;
 
+import com.example.tossback.common.dto.CommonResponse;
+import com.example.tossback.member.dto.MemberDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("")
 public class MemberController {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public MemberController(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/join")
+    public CommonResponse join(@Validated @RequestBody MemberDTO memberDTO) {
+
+        String password = bCryptPasswordEncoder.encode(memberDTO.getPassword());
+        memberDTO.setPassword(password);
+
+        return null;
+    }
+
 }
