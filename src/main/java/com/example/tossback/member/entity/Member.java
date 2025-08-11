9dabc7b55284dc6beb406cc873af3289d9255c46
DESKTOP-9Q88GKI\OVER <suong22@naver.com>
2025-08-11 15:42:55 +0900

    회원가입 경로 수정

--- src/main/java/com/example/tossback/member/entity/Member.java
+++ src/main/java/com/example/tossback/member/entity/Member.java
@@ -14,7 +14,7 @@ public class Member extends BaseEntity {
     private long id;
     @Column(unique = true, nullable = false)
     private String userId;
-    @Column(name = "username")
+    @Column(name = "username", columnDefinition = "VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
     private String username;
     private String password;
     private String email;
