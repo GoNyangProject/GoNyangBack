92a21271d069c6091240c025461364a828cd4587
DESKTOP-9Q88GKI\OVER <suong22@naver.com>
2025-08-22 16:04:02 +0900

    병합 과정에서 수정

--- src/main/java/com/example/tossback/TossBackApplication.java
+++ src/main/java/com/example/tossback/TossBackApplication.java
@@ -2,8 +2,10 @@ package com.example.tossback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TossBackApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/tossback/mypage/book/entity/Menu.java
+++ src/main/java/com/example/tossback/mypage/book/entity/Menu.java
@@ -17,12 +17,10 @@ public class Menu {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "name", columnDefinition = "VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
     private String name;
 
     private int price;
 
-    @Column(name = "content", columnDefinition = "VARCHAR(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
     private String content;
 
     @Column(precision = 3, scale = 2)  // 최대 3자리 중 소수점 2자리
@@ -30,7 +28,7 @@ public class Menu {
 
     private int bookCount;
 
-    @OneToMany(mappedBy = "menu")
+    @OneToMany(mappedBy = "menu", fetch = FetchType.LAZY)
     List<Book> books = new ArrayList<>();
 
 }
